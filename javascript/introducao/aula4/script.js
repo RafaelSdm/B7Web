b1d46988097a0c5794a2eb42c3bfed319ffb1bea
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-13 15:20:04 -0300

    Aula 4 - variaveis

---
+++ javascript/introducao/aula4/script.js
@@ -0,0 +1,14 @@
+var idade = 90;
+var nome = 'rafael';
+var sobrenome = 'damasceno'
+
+var x =10;
+var y = 70;
+
+var soma = y + x;
+var nomeCompleto = nome +  sobrenome;
+
+window.alert(idade)
+window.alert(nome)
+window.alert(soma)
+window.alert(nomeCompleto)
