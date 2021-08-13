11e5d17496eaa851152f12cd216ac0618982615a
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-13 17:58:13 -0300

    Aula 1 - Seleção de elementos com javascript

---
+++ javascript/DOM/aula1/script.js
@@ -0,0 +1,10 @@
+window.alert("UPPEERCASE?")
+document.getElementById("exemplo").innerHTML = "RAFAEL DE SOUZA DAMASCENO <strong> ABC </strong>"
+
+document.getElementsByClassName("lista")[0].innerHTML = 'alterado com sucesso'
+
+document.getElementsByTagName("button").innerHTML = "BOTAO"
+
+document.getElementsByName("email")
+
+document.querySelectorAll("#exemplo")
