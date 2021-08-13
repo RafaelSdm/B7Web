47cdc2211fe45de2827d5a9981f65eb48f8a9b5e
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-13 15:08:11 -0300

    Aula 3 - formas de mostrar Output

---
+++ javascript/introducao/aula3/script.js
@@ -0,0 +1,10 @@
+document.getElementById("titulo").innerHTML += ", Tenha um bom dia";
+
+
+document.write("algum texto de exemplo");
+
+
+window.alert("Mensagem de teste do window");
+
+
+console.log("seja bem vindo!");
