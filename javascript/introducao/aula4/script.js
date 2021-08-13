06b60a5899ee7a5208c00e5775602f7776a62ca0
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-13 15:37:09 -0300

    Aula 4 - Variaveis

--- javascript/introducao/aula4/script.js
+++ javascript/introducao/aula4/script.js
@@ -8,6 +8,14 @@ var y = 70;
 var soma = y + x;
 var nomeCompleto = nome +  sobrenome;
 
+
+let nome1 = "rafael1";
+const nome2 = "rafael";
+
+// var == HOISTING ==Tenho acesso em qualquer area do sistema
+// let = apenas uma parte do sistema ela funciona
+
+// não pode mudar o seu valor futuramente
 window.alert(idade)
 window.alert(nome)
 window.alert(soma)
