19c809d0fa6bb9d1b2ab1c4aeb9334f5a7097fdf
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-16 10:09:45 -0300

    Aula 3 - Tipo de dados e comentarios

---
+++ javascript/DOM/aula3/script.js
@@ -0,0 +1,18 @@
+var nome  = "rafael";
+var idade = 19;
+
+var salve = true;
+var cidade;
+
+console.log(cidade)
+console.log(salve)
+
+typeof(nome)
+
+var tipo = typeof(nome)
+
+// comentatio de uma linha
+
+/* comentario de multiplas linhas */
+
+console.log(tipo)
