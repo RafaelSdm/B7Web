85b486cd041b078f47200c3e4a9ef2e2f8070bd3
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-18 10:09:26 -0300

    Aula 2 - metodos para Numbers

---
+++ javascript/manipulacoes/aula2/script.js
@@ -0,0 +1,21 @@
+let n = 10;
+
+let m = 10.234334;
+
+let tostr = n.toString();
+
+let flo = m.toFixed(2)
+
+let k = '23';
+
+let soma = parseInt(23) + 2;
+
+
+
+console.log(n)
+
+console.log(tostr)
+
+console.log(flo)
+
+console.log( soma)
