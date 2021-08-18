6d2d7658945a97d69a5c93c4eb83eed7efae9cdc
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-18 10:31:19 -0300

    Aula 3 - arrays pt1

---
+++ javascript/manipulacoes/aula3/script.js
@@ -0,0 +1,52 @@
+let lista = ['ovo', 'farinha', 'corante'];
+
+let tostr = lista.toString();
+
+let jin = lista.join(',')
+
+let res = lista.indexOf('corante');
+
+let popi = lista.pop();
+
+let shif = lista.shift();
+
+
+lista.push('massa')
+
+console.log(lista)
+
+console.log(tostr)
+
+console.log(jin)
+
+console.log(res)
+
+console.log(popi)
+
+console.log(shif)
+
+
+
+let lista1 = ['ovo', 'farinha', 'corante'];
+
+let lista2 = ['ovo1', 'farinha2', 'corante3'];
+
+//lista1.splice(1, 1);
+
+
+
+//let res1 =lista1.;
+
+//let res1 = lista1.concat(lista2)
+
+
+lista1.sort();
+lista1.reverse();
+
+let res1 = lista1
+
+console.log(res1)
+
+
+
+
