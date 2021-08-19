05ef993552805e560af4904fa5072546d909c7ae
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-19 09:09:51 -0300

    Aula 4 - Datas

---
+++ javascript/manipulacoes/aula4/script.js
@@ -0,0 +1,26 @@
+let d = new Date();
+
+
+
+//let novo = d.getFullYear();
+
+//let novo = d.getMonth();
+
+//let novo = d.getDay();
+
+//let novo = d.getHours();
+
+//d.setFullYear(2022);
+
+//d.setMonth(10);
+
+d.setDate(d.getDate() + 5)
+
+let novo = d;
+
+
+
+
+
+
+console.log(novo);
