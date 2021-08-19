40cd52aef16b8cd8bc07393dbe03e9bf38988bd9
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-19 10:57:02 -0300

    Aula 9 - Desconstruindo arrays

---
+++ javascript/manipulacoes/aula9/script.js
@@ -0,0 +1,7 @@
+let array = ['rafael damasceno', 'rafael', 'damasceno', '@rafael']
+
+
+let [ nomeCompleto, nome, sobrenome, intagram ] = array
+
+console.log(nomeCompleto, nome, sobrenome, intagram)
+
