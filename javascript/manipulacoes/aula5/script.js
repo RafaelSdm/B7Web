4bbc78f453d94daaecf98949c83225b0809c4998
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-19 09:18:20 -0300

    Aula 5 - funcoes matematicas

---
+++ javascript/manipulacoes/aula5/script.js
@@ -0,0 +1,19 @@
+//let novo = Math.round(3.67);
+
+//let novo = Math.floor(3.67)
+
+//let novo = Math.ceil(3.01);
+
+//let novo =Math.abs(-7.879)
+
+//let novo = Math.min(5,6,7,6,5,4,33,4,1);
+
+//let novo = Math.max(4,5,6,66,788,1,2,3)
+
+let novo =Math.floor(Math.random() *100);
+
+
+
+
+
+console.log(novo);
