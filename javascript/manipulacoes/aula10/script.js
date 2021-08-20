7cf9466ae62857043c663e4f4a16dd948de6881f
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-20 14:32:49 -0300

    Arrow Functions

---
+++ javascript/manipulacoes/aula10/script.js
@@ -0,0 +1,33 @@
+function somar(a,b){
+    return a + b;
+}
+
+console.log(somar(5,6));
+
+
+let s = function(x,y){
+    return x+y;
+
+}
+
+console.log(s(7,8));
+
+
+let sm = (g,f) => {
+    return g + f
+}
+
+console.log(sm(3,3));
+
+
+let soma = (i,j) => i+j;
+
+console.log(soma(1,1))
+
+
+let letrasNome = (nome) =>{
+    return nome.length;
+}
+
+console.log(letrasNome('rafael'));
+
