86559ce3ab237a9e232aeba15bcb7a16bece65cc
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-20 15:19:20 -0300

    Aula 14 - values, keys e entries

---
+++ javascript/manipulacoes/aula14/script.js
@@ -0,0 +1,20 @@
+let lista = [1,2,3,4];
+
+console.log(typeof lista)
+
+
+let lista1 = ['ovo','pipoca','maccarao' ]
+
+console.log(Object.keys(lista1));
+console.log(Object.values(lista1));
+console.log(Object.entries(lista1));
+
+
+let pessoa ={
+    nome: 'rafael',
+    sobre: 'damascenp',
+    idade: 34
+}
+
+
+console.log(Object.entries(pessoa))
