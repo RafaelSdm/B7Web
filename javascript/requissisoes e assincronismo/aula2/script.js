9a77b809413e1592a50ef623983d0f486e353660
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-24 09:00:42 -0300

    Aula 2 - Callbacks

---
+++ javascript/requissisoes e assincronismo/aula2/script.js
@@ -0,0 +1,15 @@
+
+function alertar(){
+    window.alert("testando o alert");
+}
+
+
+let nome = 'rafael';
+
+setTimeout(alertar, 2000);
+
+let sob = 'damasceno';
+
+let com = `${nome} ${sob}`;
+
+console.log(`${com}`);
