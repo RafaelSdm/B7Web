efaf89a61449eefb9f508767319fef19e5d2292f
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-24 08:55:30 -0300

    Aula 1 - sincrono vs assincrono

---
+++ javascript/requissisoes e assincronismo/aula1/script.js
@@ -0,0 +1,20 @@
+let nome = 'rafael';
+let sobrenome = 'damasceno';
+
+
+let completo = `${nome} ${sobrenome}`;
+
+// codigo sincrono
+
+// codigo assincrono
+
+let nome1 = 'rafael';
+let sobrenome1 = 'damasceno';
+let temp = maquina.pegarTemperatura(); // linha assincrona
+
+let completo1 = `${nome} ${sobrenome}`;
+
+
+
+
+
