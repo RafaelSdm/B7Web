35e0a650d71de1627b732f82072408428a22aa6c
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-30 10:53:06 -0300

    Finalizando as descriçoes e imagens na tela com js

--- javascript/projeto pizzas/script.js
+++ javascript/projeto pizzas/script.js
@@ -1,3 +1,6 @@
+
+let modalQt = 1;
+
 const c = (el) =>{
     return document.querySelector(el);
 }
@@ -22,12 +25,28 @@ pizzaJson.map((item, index) =>{
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
 
+        modalQt = 1;
+
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+        c('.pizzaInfo--size.selected').classList.remove( 'selected');
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) =>{
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+
+           
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        })
         
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+
+
         c('.pizzaWindowArea').style.opacity = 0;
 
         c('.pizzaWindowArea').style.display ='flex';
