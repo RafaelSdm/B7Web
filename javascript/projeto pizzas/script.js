0e8eac8bfe77f157d1a9443f6cf88ab9cf65084b
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-30 10:32:48 -0300

    Adicionando informacoes no cardapio do produto

--- javascript/projeto pizzas/script.js
+++ javascript/projeto pizzas/script.js
@@ -10,6 +10,8 @@ pizzaJson.map((item, index) =>{
 
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
+
   
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -19,6 +21,12 @@ pizzaJson.map((item, index) =>{
 
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
+
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         
         c('.pizzaWindowArea').style.opacity = 0;
 
