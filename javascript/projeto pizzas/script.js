efc57a8c8fb132df81485fa860601b8d35f47b2f
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-09-06 08:54:12 -0300

    Aplicando os calculos de preco total e desconto

--- javascript/projeto pizzas/script.js
+++ javascript/projeto pizzas/script.js
@@ -155,22 +155,29 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
 
  //   console.log(`quantidade: ${modalQt}`);
 
-    updateCar();
+    updateCart();
     closeModal();
 
 
 
 });
 
-function updateCar(){
+function updateCart(){
 
     if(cart.length >0){
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+
+        let subtotal =0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart){
             let pizzaItem = pizzaJson.find((item)=>{
                 return item.id == cart[i].id;
                 })
+            subtotal +=  pizzaItem.price * cart[i].qt;
+
             
             let cartItem = c('.models .cart--item').cloneNode(true);
             
@@ -197,6 +204,21 @@ function updateCar(){
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                }else{
+                    cart.splice(i, 1);
+                }
+
+
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++;
+                updateCart();
+            })
 
             c('.cart').append(cartItem);
 
@@ -205,6 +227,12 @@ function updateCar(){
            // console.log(pizzaItem);
         }
 
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
 
     }else{
         c('aside').classList.remove('show');
