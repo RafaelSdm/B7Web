d9b7c6658faf24768968ce1713dbdebdb4f3d2bf
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-09-08 09:39:02 -0300

    Exibindo erros

--- javascript/projeto formulario/javascript/script.js
+++ javascript/projeto formulario/javascript/script.js
@@ -8,15 +8,19 @@ let validator = {
 
         let inputs = form.querySelectorAll('input');
 
+
+        validator.clearErrors();
+
         for(let i =0; i<inputs.length;i++){
             let input = inputs[i];
-            console.log(input);
+           // console.log(input);
             let check = validator.checkInput(input);
 
             if(check !== true){
                 send = false;
                 // exibir o erro
                 console.log(check);
+                validator.showError(input, check);
             }
         }
 
@@ -57,6 +61,30 @@ let validator = {
         return true;
 
 
+    },
+
+    showError:(input, error)=>{
+        input.style.borderColor = 'red';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibline);
+    },
+
+    clearErrors:()=>{
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let j =0; j<inputs.length;j++){
+            inputs[j].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i =0; i<errorElements.length;i++){
+            errorElements[i].remove();
+        }
     }
 
 }
