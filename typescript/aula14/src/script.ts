7224b08688bcb79b93f130eef5ed12f34c7fb036
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-11 18:39:48 -0300

    aula 14 - types para funcoes

---
+++ typescript/aula14/src/script.ts
@@ -0,0 +1,16 @@
+type mathFunction = (n1: number, n2: number) => number;
+
+
+const somar: mathFunction = (n1, n2) =>{
+    return n1 + n2;
+}
+
+const sub: mathFunction = (n1,n2) =>{
+    return n1- n2;
+}
+
+
+const mult: mathFunction = (n1, n2) =>{
+    return n1 * n2;
+
+}
