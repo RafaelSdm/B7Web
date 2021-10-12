4a59632d8fabe18f8774801895ad08ecefbb0d3a
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-12 16:32:43 -0300

    Aula 23 - Configurações para Qualidade do código e finalização do curso

---
+++ typescript/aula23/src/script.ts
@@ -0,0 +1,17 @@
+console.log("123");
+
+let nome: string = 'rafael';
+
+function somar(n1: number, n2: number, n3?: number): number{
+    let nome: string = 'rafael';
+
+    //return n1 + n2;
+
+    if(n1 > 10){
+        return n1 + n2;
+    }else{
+        return 0;
+    }
+}
+
+somar(10,14,34)
