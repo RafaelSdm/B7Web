c0324fd117ac32f8225cfff596b88ed3f87cb6b7
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-12 14:16:17 -0300

    aula 17 - Criando o arquivo tsconfig.json

---
+++ typescript/aula17/src/script.js
@@ -0,0 +1,7 @@
+"use strict";
+var nome = 'rafael';
+function teste(n1) {
+    return n1 + 5;
+}
+teste(344);
+var c = 23;
---
+++ typescript/aula17/src/script.ts
@@ -0,0 +1,12 @@
+const nome: string = 'rafael';
+
+
+function teste(n1: number){
+
+    return n1+ 5;
+
+}
+
+teste(344);
+
+const c: number = 23;
