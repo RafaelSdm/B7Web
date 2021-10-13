238f241c5a436e942fbc49a657d71d5ccbaafbf7
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-13 15:44:33 -0300

    aula 8 - Usando Nodemon com Typescript

---
+++ node/aula8/src/index.ts
@@ -0,0 +1,19 @@
+
+
+import validator from 'validator'
+
+
+console.log(validator.isEmail('rafaeldamasceno@gmail.com'));
+
+let ip = '192.168.0.1';
+
+console.log(validator.isIP(ip));
+
+console.log(validator.isUppercase("rafael"))
+console.log(validator.isUppercase("RAFAEL"))
+
+
+
+
+
+
