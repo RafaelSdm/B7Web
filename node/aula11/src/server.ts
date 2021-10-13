752f44c4c1eaf567cb5e33d87ddaa27b2fc37691
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-13 17:48:55 -0300

    aula 11 - Separando as rotas no seu lugar

---
+++ node/aula11/src/server.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from 'express'
+
+import mainRoutes  from './routes/index'
+import painelRoutes from './routes/painel'
+
+
+const server = express()
+
+
+
+
+server.use('/',mainRoutes);
+
+server.use('/painel',painelRoutes);
+
+
+
+
+
+
+server.listen(80);
+
