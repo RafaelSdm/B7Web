45839d9199f5377a530c11680f59d4d88c70f7a3
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-13 13:48:08 -0300

    aula4 - Entendendo import/export em CommonJS

---
+++ node/aula4/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const matematica = require('./matematica');
+let n1 = 10;
+let n2 = 30;
+console.log(`soma: ${matematica.somar(n1, n2)} `);
+console.log(`multiplicação: ${matematica.multiplicar(n1, n2)} `);
+console.log(`subtração: ${matematica.subtrair(n1, n2)}`);
---
+++ node/aula4/src/index.ts
@@ -0,0 +1,12 @@
+const matematica = require('./matematica');
+
+
+let n1: number = 10;
+let n2: number = 30;
+
+
+
+
+console.log(`soma: ${matematica.somar(n1,n2)} `);
+console.log(`multiplicação: ${matematica.multiplicar(n1,n2)} `);
+console.log(`subtração: ${matematica.subtrair(n1,n2)}`)
