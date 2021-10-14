6ab4aa3f54c0b9b95d8b2dae407c9c9640d97cb4
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-14 18:46:55 -0300

    Aula 22 Trabalhando com Controllers (MVC)

---
+++ node/aula22/src/server.ts
@@ -0,0 +1,50 @@
+import express, {Request, Response} from 'express'
+
+import path from 'path'
+import mustache from 'mustache-express'
+import { dirname } from 'path/posix'
+
+import mainRoutes  from './routes/index'
+import painelRoutes from './routes/painel'
+
+
+const server = express()
+
+//console.log(path.join(__dirname, '../public'));
+
+server.set('view engine', 'mustache')
+
+server.set('views', path.join(__dirname, 'views') )
+
+server.engine('mustache', mustache());
+
+
+server.use(express.urlencoded({extended: true}))
+
+
+
+
+
+server.use(express.static(path.join(__dirname, '../public')))
+
+
+
+
+server.use('/',mainRoutes);
+
+server.use('/painel',painelRoutes);
+
+
+
+
+server.use((req: Request, res: Response)=>{
+
+    res.status(404).send('Pagina nao encontrada');
+    
+
+})
+
+
+
+server.listen(80);
+
