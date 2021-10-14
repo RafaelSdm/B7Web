617a7d99ac02452e98c034255e1005a7097051fc
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-14 13:27:47 -0300

    pasta modelo

--- node/modelo/src/server.ts
+++ node/modelo/src/server.ts
@@ -1,16 +1,34 @@
 import express, {Request, Response} from 'express'
 
+import path from 'path'
+import { dirname } from 'path/posix'
+
+import mainRoutes  from './routes/index'
+import painelRoutes from './routes/painel'
+
 
 const server = express()
 
-
-server.get('/', (req: Request, res: Response) =>{
-
-    res.send("hello world");
+//console.log(path.join(__dirname, '../public'));
 
 
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('/',mainRoutes);
+
+server.use('/painel',painelRoutes);
+
+
+
+
+server.use((req: Request, res: Response)=>{
+
+    res.status(404).send('Pagina nao encontrada');
+    
 
 })
+
+
 
 server.listen(80);
 
