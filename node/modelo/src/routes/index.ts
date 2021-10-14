9e534f8aec3efc50822432e84f226f6933d1c6ce
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-14 16:00:03 -0300

    modelo - pasta

--- node/modelo/src/routes/index.ts
+++ node/modelo/src/routes/index.ts
@@ -4,17 +4,45 @@ const router = Router();
 
 
 router.get('/', (req:Request, res: Response) =>{
-    res.render('home');
+    let user = {
+        name: 'Rafael',
+        age: 14
+    }
+
+    let showOld = false;
+
+    if(user.age > 50){
+        showOld = true
+    }
+    res.render('pages/home', {
+        user: user,
+        user1: 'damasceno' ,
+        showWelcome: true,
+        showOld: showOld,
+        products:[
+
+            {title: 'Produto X', price: 10},
+            {title: 'Produto y', price: 20},
+            {title: 'Produto w', price: 40}
+        ],
+
+        lista:[
+          
+        ]
+
+        
+        
+    } );
 })
 
 
 router.get('/contato', (req: Request, res: Response) =>{
-    res.send("formulario de exemplo");
+    res.render('pages/contato')
 })
 
 
 router.get('/sobre', (req: Request, res: Response) =>{
-    res.send("Pagina sobre");
+    res.render('pages/sobre')
 })
 
 export default router;
