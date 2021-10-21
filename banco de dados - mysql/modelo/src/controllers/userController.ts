d53cf933f7d5a61a1a6b168848f65eba0c72f7b3
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-21 13:52:05 -0300

    Alterações de usuário

--- banco de dados - mysql/modelo/src/controllers/userController.ts
+++ banco de dados - mysql/modelo/src/controllers/userController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 
+import {User} from '../models/User' 
+
+import {UserInstance} from '../models/User'
+
 export const nome = (req: Request, res: Response) => {
     let nome: string = req.query.nome as string;
     let idade: string = req.query.idade as string;
@@ -30,3 +34,75 @@ export const idadeAction = (req: Request, res: Response) => {
         mostrarIdade
     });
 };
+
+
+
+export const addIdade = async (req: Request, res: Response) =>{
+   // console.log("PARAMS", req.params);
+   let id: string = req.params.id;
+
+   let results = await User.findAll({
+       where:{
+           id: id
+       }
+   })
+
+   if(results.length > 0){
+
+        let usuario = results[0];
+
+        usuario.age++;
+
+        await usuario.save();
+
+   }
+
+
+    res.redirect('/')
+}
+
+
+export const diminuirIdade = async (req: Request, res: Response) =>{
+
+
+    let id: string = req.params.id;
+    
+    let results = await User.findAll({
+        where:{
+            id: id
+        }
+    })
+
+
+    if(results.length > 0){
+        let usuario: UserInstance = results[0];
+        
+        usuario.age--;
+
+        await usuario.save();
+    }
+
+
+
+    res.redirect('/')
+}
+
+export const excluir = async (req: Request, res: Response) =>{
+
+
+    let id: string = req.params.id;
+
+    await User.destroy({
+        where:{
+            id: id
+        }
+    })
+
+    
+
+
+
+
+
+    res.redirect('/')
+}
