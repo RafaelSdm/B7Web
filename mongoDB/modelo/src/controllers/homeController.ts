7a809098c0313dbecd1b342e5b0b7e871b47cf88
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-10-22 17:18:34 -0300

    Limitar e pular resultados

--- mongoDB/modelo/src/controllers/homeController.ts
+++ mongoDB/modelo/src/controllers/homeController.ts
@@ -21,13 +21,23 @@ export const home = async (req: Request, res: Response)=>{
    // let usuarios = await User.findById('61731414b4bb340c4509621e')
 
 
-   let usuarios = await User.find({
-       //"name.firstName": 'Rafael'
-       //interests: "programação"
+   /*
 
-      // age: {$gt: 47, $lt:70} 
+
+   let usuarios = await User.find({
+       "name.firstName": 'Rafael'
+       interests: "programação"
+
+       age: {$gt: 47, $lt:70} 
         age: {$gt: 18}
    }).sort({age: -1, name: 1}); // -1 DESC 1 ASC
+
+   */
+
+
+   let usuarios = await User.find({
+        age:{$gt: 18}
+   }).skip(1).limit(1); // pular e mostrar 
 
    // gt maior que 
    // gte maior igual 
