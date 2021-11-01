4645a9558971143ff797c129035b10341b78d9be
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-11-01 13:49:25 -0300

    Classes e Funções

--- ReactJs/modelo/react1/src/App.js
+++ ReactJs/modelo/react1/src/App.js
@@ -1,27 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
+import React from "react";
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WELLO WORLD
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+class App extends React.Component{
+
+  render(){
+    return <h1>testando</h1>
+  }
+}
+*/
+
+/*
+
+function App(){
+  return <h1>testando 2,3,4</h1>
+}
+
+*/
+
+let App = () =>{
+  return <h1>testando 3 vez</h1>
 }
 
 export default App;
