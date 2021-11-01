540723b9b2043fbf236eb93d844c88393cdaee77
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-11-01 14:04:37 -0300

    jsx

--- ReactJs/modelo/react1/src/App.js
+++ ReactJs/modelo/react1/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
 
@@ -18,8 +19,40 @@ function App(){
 
 */
 
+
+/*
+
 let App = () =>{
   return <h1>testando 3 vez</h1>
+}
+
+
+*/
+
+
+function formatarNome(user){
+  return user.nome +' '+user.sobrenome;
+}
+
+
+
+function App(){
+
+  let nome = 'Rafael';
+  let user = {
+    nome: 'joao',
+    sobrenome:'silva'
+  }
+
+  let imagem = 'https:/www.google.com.br/google.jpg';
+
+  
+  return <>
+    
+    
+    <img src={imagem}/>
+
+  </>
 }
 
 export default App;
