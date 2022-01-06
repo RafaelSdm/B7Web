c7a73d0bf70ed14f23b295b840b79d5787ee3c19
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-06 14:20:03 -0300

    Entendendo o JSX

--- React/react-vite/src/App.tsx
+++ React/react-vite/src/App.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+
+
 function App(){
   return <div>HELLO WORLD!!</div>;
 }
@@ -12,4 +15,15 @@ const App1 = () =>{
   );
   }
 
-export default App1;
+
+const App2 = () => {
+  //return React.createElement('div', null, 'Meu nome é Rafael');
+  return (
+    <div>
+      Meu nome é rafael damasceno!
+      <a href="https://google.com">Clique aqui</a>
+    </div>
+  )
+}
+
+export default App2;
