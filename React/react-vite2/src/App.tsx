4df1c1b2e5dfeba8dd2681264cc885b90338b02e
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-07 13:17:12 -0300

    usando Usestate

--- React/react-vite2/src/App.tsx
+++ React/react-vite2/src/App.tsx
@@ -1,16 +1,35 @@
+
+import {useState} from 'react'
+
+
 const App = () =>{
+
+  const [numero,  setNumero] = useState(0)
+  const[name, setName] = useState('Joao');
 
 
   const handleButtonClick = () => {
 
     window.alert("o botao foi clicado!")
+    setNumero(numero + 10);
+
+  }
+
+
+  const changeName = () => {
+
+    setName('Rafael');
 
   }
 
   return(
 
     <div>
+      O numero é: {numero}
+      <br />
       <button onClick={handleButtonClick} >Clique Aqui</button>
+      <button onClick={changeName}>Clique aqui para alterar o nome</button>
+      Meu nome é: {name}
     </div>
 
   );
