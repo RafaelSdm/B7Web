6a643a2f4cd42dbca2fb458c3005d7a7fef5b262
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-10 17:49:05 -0300

    Estilizando inline

--- React/react-vite3/src/App.tsx
+++ React/react-vite3/src/App.tsx
@@ -1,1 +1,24 @@
+const App = () =>{
 
+  return(
+
+    <div>
+
+      <button style={{
+       backgroundColor: 'gold',
+       color:'blue',
+       border: 'none'
+       
+       
+       }}>Clique Aqui</button>
+
+    </div>
+
+
+  );
+
+
+
+}
+
+export default App;
