b139a70512523ab66b8491652923b132a51a4ae4
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-10 17:59:28 -0300

    Estilizando inline dinamicamente

--- React/react-vite3/src/App.tsx
+++ React/react-vite3/src/App.tsx
@@ -1,16 +1,35 @@
+
+
+import {useState} from 'react'
+
+
 const App = () =>{
+
+  const [clicked, setClicked] = useState(false)
+
+  const [padding, setPadding] = useState(0)
+
+
+  const handleClick = () =>{
+    setClicked(true);
+    setPadding(20);
+  }
 
   return(
 
     <div>
 
       <button style={{
-       backgroundColor: 'gold',
+        
+       backgroundColor: clicked ?'gold' : 'red',
        color:'blue',
-       border: 'none'
+       border: 'none',
+       width: '40rem',
+       height:'4rem',
+       padding: padding ? '5rem' : '2rem',
        
        
-       }}>Clique Aqui</button>
+       }} onClick={handleClick} >Clique Aqui</button>
 
     </div>
 
