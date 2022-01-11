a701b3c89d4e6f804bbb0d174db2ac317b4ff7c6
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-11 17:47:04 -0300

    Usando o TailwindCSS na prática

--- React/react-vite4/src/App.tsx
+++ React/react-vite4/src/App.tsx
@@ -1,8 +1,23 @@
 const App = () =>{
   return(
-    <div className="bg-black">
-      ...
+
+    <div>
+
+    <div className="bg-blue-900 text-red-300 p-6">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione totam quam amet eligendi id reiciendis odio molestiae, maiores dolores voluptates voluptate porro nihil non asperiores sit velit inventore! Sed, expedita.
     </div>
+
+
+    <div className="bg-red-400 text-white px-6 py-5 mt-8 font-bold" >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ipsum cum. Architecto, delectus ex sed saepe, quas rerum suscipit quasi placeat labore dolores ipsum laboriosam consequuntur libero. Nesciunt, placeat doloremque.
+    </div>
+
+    <button className="bg-green-300 text-black p-2 rounded-xl">clique</button>
+
+
+    </div>
+
+   
   )
 }
 
