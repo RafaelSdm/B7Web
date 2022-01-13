6a0096d98250ceea04ba036a866cd1315683e245
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-13 13:43:38 -0300

    Ciclo de vida do componente

--- React/modulo4/react-vite5/src/App.tsx
+++ React/modulo4/react-vite5/src/App.tsx
@@ -1,8 +1,29 @@
+
+import {useState, useEffect} from 'react'
+
+
+
 const App = () =>{
+
+  //let nome: string = 'Rafael';
+
+  const [name, setName] = useState('Rafael')
+
+
+  useEffect(()=>{
+    alert("executou")
+  }, [name])
+
+
+  const handleClick = () =>{
+    setName('Pedro')
+  }
+
 
   return(
     <div className="bg-red-500">
-      ....
+      Nome: {name} <br />
+      <button onClick={handleClick}>Clique aqui</button>
     </div>
   )
 
