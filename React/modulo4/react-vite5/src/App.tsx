bacda64cee419cce4431066770d46f6c5f5d5d7c
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-13 16:52:26 -0300

    Usando async/await no React

--- React/modulo4/react-vite5/src/App.tsx
+++ React/modulo4/react-vite5/src/App.tsx
@@ -88,6 +88,12 @@ const App = () =>{
 */
 
 
+
+
+/*
+
+// REQUISIÇÕES GET
+
 const App =() =>{
 
   const[movies, setMovies] = useState<Movie[]>([]);
@@ -130,6 +136,73 @@ const App =() =>{
   )
 
 }
+
+*/
+
+
+const App =() =>{
+
+  const[movies, setMovies] = useState<Movie[]>([]);
+
+
+  useEffect(() =>{
+    loadMovies();
+  }, [])
+
+  /*
+
+  const loadMovies = () =>{
+    fetch('https://api.b7web.com.br/cinema/')
+    .then((response) =>{
+
+      return response.json();
+
+
+
+    })
+
+    .then((json) =>{
+      setMovies(json)
+    });
+
+
+    window.alert("teste de alert")
+  }
+
+  */
+
+  const loadMovies = async () =>{
+    let response = await fetch('https://api.b7web.com.br/cinema/')
+    let json  = await response.json();
+
+    setMovies(json)
+    
+  
+
+
+    //window.alert("teste de alert")
+  }
+
+  return(
+    <div>
+      <button className='block bg-blue-400 p-4 rounded' onClick={loadMovies}>Carregar filmes</button>
+      Total de filmes: {movies.length}
+      <div className='grid grid-cols-6 gap-3'>
+
+        {movies.map((item, index) =>(
+          <div key={index}>
+            <img src={item.avatar} className='w-32 block' alt="" />
+            {item.titulo}
+          </div>
+        ))}
+
+      </div>
+    </div>
+  )
+
+}
+
+
 
 export default App;
 
