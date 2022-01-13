ed0a071c9b5e1958c169d91282ec477781900a50
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-13 17:23:16 -0300

    lidando com erros de requisição

--- React/modulo4/react-vite5/src/App.tsx
+++ React/modulo4/react-vite5/src/App.tsx
@@ -192,6 +192,10 @@ const App =() =>{
 
 
 
+/*
+
+
+// lIDANDO COM O LOADING
 
 const App =() =>{
 
@@ -272,6 +276,107 @@ const App =() =>{
 
 }
 
+*/
+
+
+const App =() =>{
+
+  const[movies, setMovies] = useState<Movie[]>([]);
+
+  const [loading, setLoading] = useState(false)
+
+
+  useEffect(() =>{
+    loadMovies();
+  }, [])
+
+ 
+
+  const loadMovies = async () =>{
+    try{
+    setLoading(true);
+    let response = await fetch('https://apddi.b7web.com.br/cinema/')
+    let json  = await response.json();
+    setLoading(false);
+
+    setMovies(json)
+
+    }catch(e){
+
+      setLoading(false)
+      window.alert("ERROR AO CARREGAR A PAGINA")
+      console.log(e);
+
+    }
+    
+  
+
+
+    
+  }
+
+  return(
+    <div>
+      <button className='block bg-blue-400 p-4 rounded' onClick={loadMovies}>Carregar filmes</button>
+     
+
+
+
+
+      {loading && 
+        <div>Carregando...</div>
+      
+      
+      }
+
+
+      {!loading && movies.length>0 &&
+      
+        <div>total de filmes: {movies.length}</div>
+      
+      }
+
+      {!loading && movies.length > 0 &&
+
+        <>
+
+                <div className='grid grid-cols-6 gap-3'>
+
+                {movies.map((item, index) =>(
+                  <div key={index}>
+                    <img src={item.avatar} className='w-32 block' alt="" />
+                    {item.titulo}
+                  </div>
+                ))}
+
+                </div>
+                
+        
+        
+        
+        </>
+      
+      
+      
+      }
+
+
+      {!loading && movies.length ===0 &&
+
+        <div>
+          Tente novamente mais tarde
+        </div>        
+      
+      
+      
+      }
+
+    </div>
+
+      
+  )
+
+}
 
 
 export default App;
