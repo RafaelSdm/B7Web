a7704e464b65d890ee1048d7b76f7e14815ddf4c
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-13 13:55:29 -0300

    Usando useEffect na prática

--- React/modulo4/react-vite5/src/App.tsx
+++ React/modulo4/react-vite5/src/App.tsx
@@ -1,7 +1,10 @@
 
-import {useState, useEffect} from 'react'
+import {useState, useEffect, ChangeEvent} from 'react'
 
 
+/*
+
+//CILCO DE VIDA DOS COMPONENTES
 
 const App = () =>{
 
@@ -27,6 +30,45 @@ const App = () =>{
     </div>
   )
 
+}
+
+*/
+
+const App = () =>{
+
+  const [name, setName] = useState('')
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+
+
+  useEffect(() => {
+    setFullName(`${name} ${lastName}`)
+  }, [name, lastName])
+
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) =>{
+
+    setName(e.target.value);
+
+  }
+
+  const handleLastChange = (e:ChangeEvent<HTMLInputElement>) =>{
+
+    setLastName(e.target.value);
+
+  }
+
+
+
+
+  return(
+    <div className='flex flex-col'>
+      <input type="text" value={name} placeholder='Informe seu nome:' onChange={handleNameChange} />
+      <input type="text" value={lastName} placeholder='Informe seu sobrenome' onChange={handleLastChange} />
+      <p>Nome completo: {fullName}</p>
+    </div>
+  )
 }
 
 export default App;
