e58cc9f7f0d379626d7aa561f57a7189af6e73a2
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-14 18:04:13 -0300

    Introsução aos reducers

--- React/modulo4/react-vite6/src/App.tsx
+++ React/modulo4/react-vite6/src/App.tsx
@@ -1,8 +1,63 @@
 
+import {useReducer} from 'react'
+
+
+
+type reducerState = {
+  count: number;
+}
+
+type reducerAction = {
+  type: string;
+
+
+}
+
+const initialState = {count:0};
+
+
+const reducer = (state:reducerState, action: reducerAction) =>{
+
+  switch(action.type){
+    case 'ADD':
+      return {...state, count: state.count + 1};
+      break;
+
+    case 'DEL':
+      return{...state, count: state.count - 1};
+      break;
+
+    case 'RESET':
+      return initialState;
+      break;
+  }
+
+
+
+  return state;
+
+
+}
+
+
+
+
 const App = () =>{
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+
   return(
-    <div className="bg-blue-500">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus officiis non, dolore sint mollitia reprehenderit tempore laudantium quisquam. Mollitia laudantium quam quo asperiores repellendus dolorem consequatur cupiditate sapiente? Provident, doloribus.
+    <div className="bg-blue-500 p-5">
+
+      Contagem : {state.count}
+      <hr />
+      <button className='p-3 border bg-white' onClick={() => dispatch({type: 'ADD'}) }>Adiconar</button>
+      <button className='p-3 border bg-white' onClick={() => dispatch({type: 'DEL'}) }>Deletar</button>
+      <button className='p-3 border bg-white' onClick={() => dispatch({type: 'RESET'}) }>Resear</button>
+
+
+      
     </div>
   )
 }
