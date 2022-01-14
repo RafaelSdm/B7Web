bf1f28ee0acf17522a8cf6ee9d14c0b12df3dcc5
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-14 17:43:20 -0300

    commit full course

--- React/modulo4/react-vite6/src/App.tsx
+++ React/modulo4/react-vite6/src/App.tsx
@@ -1,45 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+const App = () =>{
+  return(
+    <div className="bg-blue-500">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus officiis non, dolore sint mollitia reprehenderit tempore laudantium quisquam. Mollitia laudantium quam quo asperiores repellendus dolorem consequatur cupiditate sapiente? Provident, doloribus.
     </div>
   )
 }
 
-export default App
+
+
+export default App;
