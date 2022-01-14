ebf2a5527cc365b1d6acfba78b2fb140dee947f0
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-14 17:26:39 -0300

    usando axios

--- React/modulo4/react-vite5/src/api.ts
+++ React/modulo4/react-vite5/src/api.ts
@@ -1,3 +1,6 @@
+import axios from 'axios'
+
+/*
 
 const BASE = 'https://jsonplaceholder.typicode.com';
 
@@ -32,6 +35,43 @@ export const api = {
 
       let json = await response.json();
       return json
+
+    }
+}
+
+*/
+
+
+
+
+//USANDO AXIOS
+
+//const BASE = 'https://jsonplaceholder.typicode.com';
+
+
+const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+})
+
+export const api = {
+    getAllPosts: async () =>{
+
+        let response = await axiosInstance.get(`/posts`)
+        return response.data;
+
+        
+
+    },
+    addNewPost: async (title:string, body:string, userId:number) =>{
+
+        let response = await axiosInstance.post(`/posts`, {
+            title, body, userId
+        });
+
+
+        return response.data;
+
+        
 
     }
 }
