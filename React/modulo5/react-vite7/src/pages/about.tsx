460271909be8f4b8c4117e9c82611465c2bb2551
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-02 17:34:56 -0300

    Parametros de query

--- React/modulo5/react-vite7/src/pages/about.tsx
+++ React/modulo5/react-vite7/src/pages/about.tsx
@@ -1,8 +1,28 @@
-import {Link} from 'react-router-dom'
+import {Link, useSearchParams} from 'react-router-dom'
 
 export const About = () =>{
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+
+    const setOrder = (order: 'asc' | 'desc') =>{
+
+        searchParams.set('order', order)
+
+        setSearchParams(searchParams)
+
+    }
     return(
         <div>
+            Filtro: {searchParams.get('filter')} 
+            <br />
+            Ordem: {searchParams.get('order')}
+
+            <hr />
+
+            <button onClick={() => setOrder('asc')}>Asc</button>
+            <button onClick={() => setOrder('desc')}>Desc</button>
+
             <ul>
                 <li><Link to="/about/rafael">Rafael</Link></li>
                 <li><Link to="/about/jose">Jose</Link></li>
