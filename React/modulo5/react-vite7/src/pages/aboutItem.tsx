b6fb16c0ba59c099b8601d60677656531ebd20ed
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-02 16:34:12 -0300

    redirecionamento de rotas

--- React/modulo5/react-vite7/src/pages/aboutItem.tsx
+++ React/modulo5/react-vite7/src/pages/aboutItem.tsx
@@ -1,13 +1,26 @@
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 
 
 export const AboutItem = () =>{
 
     const params = useParams();
+    const navigate = useNavigate();
+
+    const handleBackButton = () =>{
+        navigate(-1);
+    }
+
+    const handleBackHome = () =>{
+        navigate('/')
+    }
 
     return(
         <div>
             Pagina sobre {params.slug?.toUpperCase()} ({params.slug?.length} letras)
+            <hr />
+            <button onClick={handleBackButton}>Voltar</button>
+            <br />
+            <button onClick={handleBackHome}>Voltar para a pagina home</button>
         </div>
     )
 }
