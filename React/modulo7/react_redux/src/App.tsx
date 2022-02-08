8c4ea7c042a3a17f911f320c9c1f4ce95523f5ee
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-08 13:11:41 -0300

    modulo 7: redux na pratica

---
+++ React/modulo7/react_redux/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+
+
+const App = () =>{
+  return(
+    <div>
+      Meu nome é ... e tenho ... anos <br />
+      tema: ...
+      <hr />
+
+    <input type="text" name="" value={'...'} id="" />
+    <hr />
+    <button>Switch theme</button>
+
+    </div>
+  )
+}
+
+export default App;
